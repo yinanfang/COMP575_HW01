609efdf0b05e8ea108d39e42089ba1b927116e5f
yinanfang <yinan_fang@hotmail.com>
2014-09-18 12:07:35 -0400

    Initial Commit

---
+++ COMP575_HW01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  COMP575_HW01
+//
+//  Created by Golden Compass on 9/18/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
